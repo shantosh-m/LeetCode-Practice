0663a9dc2331f0aa6f612537e9f2f2a7096c3f94
shantosh-m <e19371@eng.pdn.ac.lk>
2026-01-15 08:33:10 +0530

    Leetcode 33 added
    "Seach in Rotated Sorted Array" problem python solution uploaded

--- 33 Seach in Rotated Sorted Array/Solution.py
+++ 33 Seach in Rotated Sorted Array/Solution.py
@@ -1,4 +1,27 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        
-
+        left: int = 0
+        right: int = len(nums)-1
+        while(left <= right):
+            mid: int = (left + right)//2
+            if(nums[mid] == target):
+                return mid
+            else:
+                if(nums[mid] < nums[right]):                # right sorted
+                    if(nums[mid] < target and target <= nums[right]):
+                        left = mid+1
+                    else:
+                        right = mid-1
+                else:                                       #left sorted
+                    if(nums[left] <= target and target <nums[mid]):
+                        right = mid-1
+                    else:
+                        left = mid+1
+        return -1
+    
+if __name__ == "__main__":
+    # Create an instance of Solution to call the method
+    sol = Solution()
+    print(sol.search([4, 5, 6, 7, 0, 1, 2], 0))  # Output: 4
+    print(sol.search([4, 5, 6, 7, 0, 1, 2], 3))  # Output: -1
+    print(sol.search([1], 0))                    # Output: -1
